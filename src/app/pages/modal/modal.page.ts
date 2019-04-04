4cd563c8e7c1cc4bf218e78ea2f40b948a9c2c4a
Arman Kostandyan <arman_k@instigatemobile.com>
2019-04-04 18:02:50 +0400

    Added functionality to scan Bluetooth LE devices and show the list

---
+++ src/app/pages/modal/modal.page.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { NavParams, ModalController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.page.html',
+  styleUrls: ['./modal.page.scss'],
+})
+export class ModalPage implements OnInit {
+
+  devices: any[];
+  constructor(private navParams: NavParams, private modalCtrl: ModalController) {
+  }
+
+  ngOnInit() {
+    this.devices = this.navParams.get('devices');
+    console.log('navParams :', this.navParams);
+    console.log('this.devices :', this.devices);
+  }
+
+  dismissModal(data?: any): void {
+    this.modalCtrl.dismiss(data);
+  }
+
+  get filteredDevices() {
+    return this.devices.filter(device => !!device.name);
+  }
+
+  public selectDevice(device: any) {
+    this.dismissModal(device);
+  }
+
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { ToastController, ModalController } from '@ionic/angular';
+import { BLE } from '@ionic-native/ble/ngx';
+import { ModalPage } from '../pages/modal/modal.page';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  devices: any[] = [];
+  statusMessage: string;
+  temp = 0;
+  selectedDevice: any;
+
+  constructor(
+    private toastCtrl: ToastController,
+    private ble: BLE,
+    private ngZone: NgZone,
+    public modalController: ModalController) {
+  }
+
+  public ionViewDidEnter() {
+    this.scan();
+  }
+
+  public async onTmpChange(event: any): Promise<any> {
+    this.temp = event.target.value;
+  }
+
+  public async devicesListClick() {
+    const modal = await this.modalController.create({
+      component: ModalPage,
+      componentProps: { devices: this.devices }
+    });
+    await modal.present();
+    const data: any = await modal.onDidDismiss();
+    this.selectedDevice = data.name;
+    await this.showToast(this.selectedDevice.name);
+  }
+
+  private async presentModal(devices: string[]) {
+    const modal = await this.modalController.create({
+      component: ModalPage,
+      componentProps: { devices: devices }
+    });
+    await modal.present();
+  }
+
+  private scan() {
+    this.setStatus('Scanning for Bluetooth LE Devices');
+    this.devices = [];  // clear list
+
+    this.ble.scan([], 5).subscribe(
+      device => this.onDeviceDiscovered(device),
+      error => this.scanError(error)
+    );
+
+    setTimeout(this.setStatus.bind(this), 10000, 'Scan complete');
+  }
+
+  private async onDeviceDiscovered(device) {
+    this.ngZone.run(() => {
+      this.devices.push(device);
+    });
+  }
+
+  // If location permission is denied, you'll end up here
+  private async scanError(error) {
+    this.setStatus('Error ' + error);
+  }
+
+  private async setStatus(message: string) {
+    this.showToast(message);
+    this.ngZone.run(() => {
+      this.statusMessage = message;
+    });
+  }
+
+  private async showToast(msg: string) {
+    const toast = await this.toastCtrl.create({
+      message: msg,
+      position: 'bottom',
+      duration: 3000
+    });
+    await toast.present();
+  }
+
+  private deviceSelected(device) {
+    console.log(JSON.stringify(device) + ' selected');
+  }
+
+}
